22716f34767759552ab6fe1d42239a397651cc9a
Ozeas Santos <ozeassantossilva@gmail.com>
2019-06-03 17:24:38 -0300

    Application entry page add: - added entry page for registration - added loading of components and pages - added input page style file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles.css';
+
+/**
+ * import ui webcomponents
+ */
+import './ui/ui-input/ui-input';
+import './ui/ui-button/ui-button';
+import './ui/ui-password-strength/ui-password-strength';
+import './ui/ui-logo-type/ui-logo-type';
+import './ui/ui-success-message/ui-success-message';
+import './ui/ui-spinner/ui-spinner';
+
+/**
+ * import scenes pages
+ */
+import './SignUpPage';
